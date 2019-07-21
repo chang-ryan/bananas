5893dbb8190b48868128f08e88b5aa9f080a5e91
Ryan Chang <ryan@sandalboyz.com>
2019-07-21 00:23:21 -0700

    stuff

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bot = new Discord.Client({})
 bot.on('ready', () => {
   console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
 
-  // bot.user.setActivity(`Serving ${bot.guilds.size} servers.`)
+  bot.user.setActivity(`Serving ${bot.guilds.size} servers.`)
 })
 
 bot.on('message', async (message) => {
@@ -22,7 +22,27 @@ bot.on('message', async (message) => {
 
   if (command === 'ping') {
     const m = await message.channel.send('Ping?')
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms.`)
+    return m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms.`)
+  }
+
+  if (command === 'enter' || command === 'ent') {
+    const direction = messageContentArray[2]
+    const entry = messageContentArray[3]
+
+    const position = {
+      direction,
+      entry
+    }
+
+    console.log(position)
+
+    // Store position into DB under the user that sent it.
+
+    return message.channel.send(`${message.author.username}, you're position has been entered. May the odds be ever in your favor.`)
+  }
+
+  if (command === 'positions' || command === 'position' || command === 'pos' || command === 'p') {
+    // Get all positions out of DB for current guild. Pretty display that shit.
   }
 })
 
