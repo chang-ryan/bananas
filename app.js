4844308e53e81603c5ae809ab25daceb843f005a
Ryan Chang <ryan@sandalboyz.com>
2019-07-22 20:58:06 -0700

    adds stuff

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`)
   console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
 
-  bot.user.setActivity(`Serving ${bot.guilds.size} servers.`)
+  bot.user.setActivity(`Eating bananas in ${bot.guilds.size} servers.`)
 })
 
 bot.on('message', async (message) => {
