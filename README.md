1bd3c0090683c64700a0c256c1f80244c42627b9
ryansandalboyz <39848716+ryansandalboyz@users.noreply.github.com>
2019-07-23 22:38:43 -0700

    V1 (#1)
    * stuff
    
    * more stuff
    
    * adds prefixes and checks for a command
    
    * adds enter
    
    * readme
    
    * adds help and babel
    
    * console logs
    
    * adds open pnl
    
    * adds stuff
    
    * adds emojis
    
    * procfile

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Bananas is a bot for [Discord](https://discord.js.org/).
 
 ## Getting Started
+
+### Install dependencies
+
+`yarn`
+
+### Developing
+
+Use `yarn dev` to start a `nodemon` instance of the application.
--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-require('dotenv').config()
+import 'dotenv/config'
 
-const Discord = require('discord.js')
-const config = require('./config.json')
+import Discord from 'discord.js'
+import Enmap from 'enmap'
+import axios from 'axios'
+import { stripIndent } from 'common-tags'
+import { get } from 'lodash'
+import config from './config.json'
 
 const bot = new Discord.Client({})
+const board = new Enmap({ name: 'board' })
+
+const LONG = 'long'
+const SHORT = 'short'
+const FLAT = 'flat'
 
 bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`)
   console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
 
-  // bot.user.setActivity(`Serving ${bot.guilds.size} servers.`)
+  bot.user.setActivity(`Eating bananas in ${bot.guilds.size} servers.`)
 })
 
 bot.on('message', async (message) => {
@@ -18,11 +28,113 @@ bot.on('message', async (message) => {
   const prefix = messageContentArray[0]
   const command = messageContentArray[1]
 
-  if (prefix !== config.prefix) return
+  if (!config.prefixes.includes(prefix)) return
+  if (!command) {
+    return message.channel.send('Did you need help? Use `!bananas help` to show what I can do.')
+  }
 
   if (command === 'ping') {
     const m = await message.channel.send('Ping?')
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms.`)
+    return m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms.`)
+  }
+
+  if (command === 'help' || command === 'h') {
+    return message.channel.send(stripIndent`
+      Howdy. This bot's prefix is \`!bananas\` or \`!bnns\`.
+
+      Commands you can use:
+
+      Enter: log yourself into a position.
+        \`enter\`, \`ent\`: \`!bananas enter <direction> <entry>\`, i.e. \`!bananas enter long 10000\`
+
+      Exit: remove yourself from a position.
+        \`exit\`, \`ex\`: \`!bananas exit\`
+
+      Positions: show all positions from your friends.
+        \`positions\`, \`position\`, \`pos\`, \`p\`: \`!bananas positions\`
+    `)
+  }
+
+  if (command === 'enter' || command === 'ent') {
+    const DIRECTIONS = [LONG, SHORT]
+
+    const direction = messageContentArray[2]
+    const entry = messageContentArray[3]
+
+    if (!direction || !entry) {
+      return message.channel.send('Entry formatted incorrectly. Use `!bananas enter <direction> <entry>` to enter your position.')
+    }
+
+    if (!DIRECTIONS.includes(direction.toLowerCase())) {
+      return message.channel.send('Usable directions are `long` and `short`.')
+    }
+
+    const position = {
+      name: message.author.username,
+      direction,
+      entry,
+      position: `${direction === SHORT ? '-' : ''}${entry}`
+    }
+
+    board.ensure(message.guild.id, {})
+    board.set(message.guild.id, position, message.author.id)
+
+    return message.channel.send(`_${message.author.username}_, you're position has been entered. May the odds be ever in your favor.`)
+  }
+
+  if (command === 'exit' || command === 'ex') {
+    const position = {
+      name: message.author.username,
+      direction: FLAT
+    }
+
+    board.set(message.guild.id, position, message.author.id)
+
+    return message.channel.send(`${message.author.username}, you have exited your position.`)
+  }
+
+  if (command === 'positions' || command === 'position' || command === 'pos' || command === 'p') {
+    const bitcoinPriceIndexResponse = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    let bitcoinPriceIndex = get(bitcoinPriceIndexResponse, 'data.bpi.USD.rate')
+
+    if (bitcoinPriceIndex) {
+      bitcoinPriceIndex = Number(bitcoinPriceIndex.replace(',', '')).toFixed(2)
+    }
+
+    const positions = board.get(message.guild.id)
+    const messages = Object.keys(positions).map((userId) => {
+      const { name, direction, entry } = positions[userId]
+
+      if (direction === FLAT) {
+        return `_${name}_ is ${FLAT} like our planet.`
+      }
+
+      let openPnl
+      let directionIcon
+
+      if (direction === LONG) {
+        openPnl = bitcoinPriceIndex - Number(entry)
+        directionIcon = '🚀'
+      }
+      if (direction === SHORT) {
+        openPnl = Number(entry) - bitcoinPriceIndex
+        directionIcon = '💩'
+      }
+
+      if (bitcoinPriceIndex) {
+        return `${directionIcon} _${name}_ is ${direction} from ${entry} for an open PNL of ${openPnl.toFixed(2)} points.`
+      }
+
+      return `${directionIcon} _${name}_ is ${direction} from ${entry}.`
+    })
+
+    if (bitcoinPriceIndex) {
+      messages.unshift(`\`BTCUSD ${bitcoinPriceIndex}\`\n`)
+    }
+
+    const response = messages.join('\n')
+
+    return message.channel.send(response)
   }
 })
 
